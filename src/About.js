85348bf51d396eae10788ab7f3727cc8cc5d6e68
Matthias Delamare <matthias.delamare@cherbourg.fr>
2025-03-09 11:32:00 +0100

    Page A propos

--- src/About.js
+++ src/About.js
@@ -1,5 +1,24 @@
 
 
-export default function About(){
-  return (<h1>A propos</h1>)
+export default function About() {
+  return (<div id="cont-apropos">
+    <h2>Pont Tournant de Cherbourg</h2>
+    <p>Le pont tournant de Cherbourg est susceptible de touner
+      <b>une heure avant</b> et <b>une heure après</b> la pleine mer.
+       Le pont ne tournera que si un bâteau se présente pour entrer ou sortir du bassin du commerce. </p>
+    <p>Pendant toute la durée de la manœuvre, la traversée du pont par les véhicules terrestres et les piétons est
+       rendue impossible. Cette manœuvre ne dure en général pas plus de dix minutes. </p>
+    <h2>A propos de l'application</h2>
+    <p>Si vous avez l'habitude de traverser le pont tournant à certaines heures (pour aller ou revenir du travail par
+       exemple),
+       renseignez ces horaires dans l'application. Vous recevrez une notification quelques minutes avant la manœuvre.
+       Ce délai est paramétrable à votre guise : vous pouvez définir combien de temps avant que la manœuvre n'ait lieu
+       vous souhaitez être prévenu. </p>
+    <p>Les informations données ici le sont à titre purement informatif. L'auteur de l'application ne peut être
+       tenu pour responsable de la qualité des prédictions, ni des conséquences de l'utilisation de cette
+       application. </p>
+    <br/>
+    <p>Développé par Matthias Delamare</p>
+    <p><a href="http://mdelamare.free.fr">http://mdelamare.free.fr</a></p>
+  </div>);
 }
