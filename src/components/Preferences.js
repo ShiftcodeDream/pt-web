7752071a7c359efb199389ef331f0277c55dd44f
Matthias Delamare <matthias.delamare@cherbourg.fr>
2025-03-10 11:09:51 +0100

    Ergonomie générale des préférences

--- src/components/Preferences.js
+++ src/components/Preferences.js
@@ -9,8 +9,6 @@ export default function Preferences(){
   const [notifDelay, setNotifDelay] = useState(10);
   const [timeRanges, setTimeRanges] = useState([]);
 
-  // TODO : save timeranges in localStorage
-
   useEffect(() => {
     setNotifEnabled(getConfigValue(NOTIF_ENABLED_KEY) === 'true');
     const delai = getConfigValue(NOTIF_DELAY_KEY);
@@ -49,16 +47,13 @@ export default function Preferences(){
     <h2>Configuration des notifications</h2>
     <p>
       <input type="checkbox" id="preventMe" checked={notifEnabled} onChange={toggleEnabled}/>
-      <label htmlFor="preventMe">Me prévenir avant que le pont tourne</label>
+      <label htmlFor="preventMe">Me prévenir</label>
+      <input type="number" value={notifDelay} step="10"
+             onChange={e => setNotifDelay(e.target.value)} min="0" max="120"/>
+      <label htmlFor="preventMe">minutes avant que le pont tourne</label>
     </p>
     <div hidden={!notifEnabled}>
-      <p className="separateur">
-        <label htmlFor="delai">Délai de prévenance en minutes</label>
-        <input type="number" value={notifDelay} onChange={e=>setNotifDelay(e.target.value)} min="0" max="120" step="10"/>
-      </p>
-      <p>
-        <button onClick={addTimeRange}>+ Ajouter</button>
-      </p>
+      <p><button onClick={addTimeRange}>+ Ajouter</button></p>
       <div id="listeAlertes">
         {timeRanges && timeRanges.length>0 && timeRanges.map((t, ind) =>
           <TimeRange value={t} key={'tr' + ind} cle={'tr' + ind}
